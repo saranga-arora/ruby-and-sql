de98061822959ccaec9440caf1251a995621bc80
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-02-01 17:01:21 +0000

    adds code

--- labs/2-associations.rb
+++ labs/2-associations.rb
@@ -3,24 +3,18 @@
 # should be the top-level directory (i.e. /workspace/your-app/) and then run:
 # rails runner labs/2-associations.rb
 
-# 1. write code to display each contact (you can reuse the previous lab's code) and include the contact's company name, e.g.:
+# 1. write code to display each contact (you can reuse the previous lab's code) and include the contact's company name:
+puts "Contacts: #{Contact.all.count}"
+all_contacts = Contact.all
+for contact in all_contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}" 
+end
 
-# ---------------------------------
-# Contacts: 4
-# Andy Jassy - andy@amazon.com - Amazon.com, Inc.
-# Craig Federighi - craig@apple.com - Apple Inc.
-# Elon Musk - elon@tesla.com - Tesla, Inc.
-# Tim Cook - tim@apple.com - Apple Inc.
-
-# 2. similar to above, but this time organized by company, write code to display each company (name) and its contacts, e.g.:
-
-# ---------------------------------
-# Apple Inc.
-# Tim Cook - tim@apple.com
-# Craig Federighi - craig@apple.com
-#
-# Amazon.com, Inc.
-# Andy Jassy - andy@amazon.com
-#
-# Tesla, Inc.
-# Elon Musk - elon@tesla.com
+# 2. similar to above, but this time organized by company, write code to display each company (name) and its contacts:
+all_companies = Company.all
+for company in all_companies
+    puts "#{company.name}"
+    for contact in company.contacts  
+        puts "#{contact.first_name} #{contact.last_name} - #{contact.email}"
+    end
+end
